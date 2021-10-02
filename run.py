8cb572938b0bac8203079aeb5dadda8dd6a64445
Ketan Puranik <69633773+KP-crypto@users.noreply.github.com>
2021-10-02 20:07:13 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
 driver.get('https://google.com')
 driver.maximize_window()
 time.sleep(2)
