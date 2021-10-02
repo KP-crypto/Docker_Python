09623f14cd94c8519ea7a1a95b97dd3635cfc8ac
Ketan Puranik <69633773+KP-crypto@users.noreply.github.com>
2021-10-02 20:29:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,8 +5,10 @@ import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
 driver.get('https://google.com')
 driver.maximize_window()
 time.sleep(2)
